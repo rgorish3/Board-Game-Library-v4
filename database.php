5151f4080de946f5c0de9627bebd7caff70c470c
rgorish3 <rgorish3@gmail.com>
2022-08-31 19:53:48 -0400

    Removed a security hole in a comment

--- database.php
+++ database.php
@@ -5,5 +5,4 @@ $dotenv = \Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
 $pdo = new PDO('mysql:host='.getenv('DB_ADDRESS'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
-//$pdo = new PDO('mysql:host=boardgamedb.crkpfrpnbimw.us-east-2.rds.amazonaws.com;port=3306;dbname=BoardGameLibrary', 'root', 'dragonsFo1!y');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
