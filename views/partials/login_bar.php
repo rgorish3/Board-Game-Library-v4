8c2bbcba942b3209ea4d9c33b39539e43b98e41e
rgorish3 <rgorish3@gmail.com>
2022-09-14 23:26:36 -0400

    Set up login_bar

--- views/partials/login_bar.php
+++ views/partials/login_bar.php
@@ -3,8 +3,20 @@
 
 <nav class="navbar navbar-expand-lg">
 <div class="container-fluid">
-<span class="navbar-text text-end">
-    Navbar text with an inline element
-</span>
+    <div class="nav-item margin-end"></div>
+        <span class="navbar-text">
+
+            <?php if(isset($_SESSION['user'])){ ?>
+
+                <h4 class="text-end">Hello <?php echo $_SESSION['user'];?>!</h4>
+                <a href="account.php" class='btn btn-primary'>Account Settings</a>
+                <a href="logout.php" class='btn btn-primary'>Log Out</a>
+            <?php }
+            else{ ?>
+                <a href="login.php" class='btn btn-primary'>Log In</a>
+            <?php } ?>
+            
+        </span>
+    </div>  
 </div>
 </nav>
