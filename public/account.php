a43297a5c3900e3265c0a9eb2f3ccba22e1bdde0
rgorish3 <rgorish3@gmail.com>
2022-10-25 23:15:50 -0400

    Added change library functionality

--- public/account.php
+++ public/account.php
@@ -29,6 +29,12 @@ if(isset($_POST['submit']))
 
 
 /*      LIBRARIES       */
+
+
+
+
+
+
 $statement = $pdo->prepare('SELECT * FROM libraries ORDER BY library');
 $statement->execute();    
 $libraries = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -42,7 +48,18 @@ $statement -> bindValue(':id',$_SESSION['id']);
 $statement->execute();
 $library = $statement->fetch(PDO::FETCH_ASSOC);
 
-$library = $library['id']
+$library = $library['id'];
+
+if(isset($_POST['submit-library']))
+{
+    if($library != $_POST['libraries_select']){
+        $libResponse=changeLibrary($_POST['libraries_select']);
+        $library = $_POST['libraries_select'];
+    }
+    
+}
+
+
 
 ?>
 
@@ -54,90 +71,87 @@ $library = $library['id']
     <?php include_once("../views/partials/login_bar.php");?>
     <div class="main ">
 
-        <p>
-            <a href="index.php" class="btn btn-secondary">Go Back to Board Game Library</a>
-            <div class="container-fluid ">
-                <div class="row justify-content-center mt-5">
 
-                    <div class="col-lg-5 col-md-10 me-10 mb-5 bg-light">
-                        <h2 class="text-center">Change Password</h2>
-            
-                            <?php
-                                if (!empty($response)){?>
-                                    <div>
-                                        <span class=<?php
-                                            if($response['status'] == 'error'){
-                                                echo "text-danger";
-                                            } 
-                                            else{ 
-                                                echo 'text-success';
-                                            }?>
-                                        ><strong><?php echo $response['message'];?></strong></span>
-                                    </div>
-                                <?php } ?>
-            
 
-                            <form method="post">
-                               <div class="form-group my-3 mx-5">
-                                    <input type="text" class="form-control" placeholder="Old Password" name="oldPassword">
+
+        <a href="index.php" class="btn btn-secondary">Go Back to Board Game Library</a>
+        <div class="container-fluid ">
+            <div class="row justify-content-center mt-5">
+                <div class="col-lg-5 col-md-10 me-10 mb-5 bg-light">
+                    <h2 class="text-center">Change Password</h2>
+        
+                        <?php
+                            if (!empty($response)){?>
+                                <div>
+                                    <span class=<?php
+                                        if($response['status'] == 'error'){
+                                            echo "text-danger";
+                                        } 
+                                        else{ 
+                                            echo 'text-success';
+                                        }?>
+                                    ><strong><?php echo $response['message'];?></strong></span>
                                 </div>
-                                <div class="form-group my-3 mx-5">
-                                    <input type="text" class="form-control" placeholder="New Password" name="newPassword1">
-                                </div>
-                                <div class="form-group my-3 mx-5">
-                                    <input type="text" class="form-control" placeholder="Retype New Password" name="newPassword2">
-                                </div>
-                                <div class="form-group mb-3 mt-3 text-end ">
-                                    <button class="btn btn-primary" type="submit" name="submit" id="button-addon2">Change Password</button>
-                                </div>
-                            </form>
-                            
+                            <?php } ?>
+        
+                        <form method="post">
+                           <div class="form-group my-3 mx-5">
+                                <input type="password" class="form-control" placeholder="Old Password" name="oldPassword">
+                            </div>
+                            <div class="form-group my-3 mx-5">
+                                <input type="password" class="form-control" placeholder="New Password" name="newPassword1">
+                            </div>
+                            <div class="form-group my-3 mx-5">
+                                <input type="password" class="form-control" placeholder="Retype New Password" name="newPassword2">
+                            </div>
+                            <div class="form-group mb-3 mt-3 text-end ">
+                                <button class="btn btn-primary" type="submit" name="submit" id="button-addon2">Change Password</button>
+                            </div>
+                        </form>
                         
-                    </div>
                     
-                    <div class="col-lg-1"></div>
+                </div>
+                
+                <div class="col-lg-1"></div>
 
-                    <div class="col-lg-5 col-md-10 mb-5 bg-light">
-                        <h2 class="text-center">Library</h2>
-                    
-                        <div class= "col-md-4 col-sm-4">
-                            <label for="library_select">Library</label>
-                            <select class="form-select" id="library_select" name="owners_select">
+                <div class="col-lg-5 col-md-10 mb-5 bg-light">
+                    <h2 class="text-center">Library</h2>
+                        <?php
+                            if (!empty($libResponse)){?>
+                                <div>
+                                    <span class=<?php
+                                        if($libResponse['status'] == 'error'){
+                                            echo "text-danger";
+                                        } 
+                                        else{ 
+                                            echo 'text-success';
+                                        }?>
+                                    ><strong><?php echo $libResponse['message'];?></strong></span>
+                                </div>
+                            <?php } ?>
 
+                        <form method="post">
+                            <select class="form-select form-group my-3 mx-5" id="library_select" name="libraries_select">
                                 <?php 
                                     foreach($libraries as $i => $indivLib):
-
                                         $str= "<option value=".$indivLib["id"];
                                         if($indivLib["id"]==$library){
                                             $str.= " selected=selected ";
                                         }
                                         $str.=">".$indivLib["library"]."</option>";
-                                        
-                                        echo $str;
 
+                                        echo $str;
                                     endforeach
                                 ?>
-
-                                
-                        </select>
+                            </select>
+                            
+                            <div class="form-group mb-3 mt-3 text-end ">
+                                <button class="btn btn-primary" type="submit" name="submit-library" id="button-addon2">Change Library</button>
+                            </div>
+                        </form>
                     </div>
-
-
-
-
-
-                        </div>
-                    </div>
-
-
                 </div>
             </div>
-        
-        </p>
-        
-
-
-
-
+        </div>
     </div>
 </body>
